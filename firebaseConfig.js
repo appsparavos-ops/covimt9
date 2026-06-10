d3d7ce9bad82289125430c3c1e3e4367accecadb
appsparavos-ops <appsparavos@gmail.com>
2026-06-09 22:23:47 -0300

    Add files via upload

---
+++ firebaseConfig.js
@@ -0,0 +1,10 @@
+// Configuración de Firebase de la Cooperativa Habitacional
+// Reemplaza estos campos con los datos de tu consola de Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyBvjEMFZTfYyoS7agAUAW5UNMbTjYdJ5A8",
+  authDomain: "obracovimt9-63b73.firebaseapp.com",
+  projectId: "obracovimt9-63b73",
+  storageBucket: "obracovimt9-63b73.firebasestorage.app",
+  messagingSenderId: "627428083648",
+  appId: "1:627428083648:web:002f42eda2fbef3966aa25"
+};
